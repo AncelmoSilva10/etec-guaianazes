c14a2c181d4c4e45ffcebf88b7c9494a0a6a2f8a
Ancelmo Silva de Sousa <142951701+AncelmoSilva10@users.noreply.github.com>
2023-09-23 20:16:41 -0300

    Update Exercicio5.java

--- TPA/Lista09/Exercicio5.java
+++ TPA/Lista09/Exercicio5.java
@@ -1,21 +1,25 @@
 import java.util.Scanner;
-public class Exercicio4 {
-
+public class Exercicio5 {
 	public static void main(String[] args) {
 		Scanner leia = new Scanner(System.in);
-		int i = 1,ant = 0,atu = 1,n, prx;
+		int i = 1, menor = 0, maior = 0, idade;
 		
-		System.out.println("Insira o Enésimo Número:");
-		n = leia.nextInt();
-		while(i<n) {
-			System.out.println(atu);
-			prx = ant + atu;
-			ant = atu;
-			atu = prx;
+		while(i<11) {
+			System.out.println("Insira a Idade do "+i+"º usuário:");
+			idade = leia.nextInt();
+			
+			if(idade<18) {
+				menor = menor + 1;
+			}else {
+				maior = maior + 1;
+			}
 			i++;
 			
 		}
-		leia.close();
+			System.out.println("Menores de Idades são "+menor);
+			System.out.println("Maiores de Idade são "+maior);
+			leia.close();
 		}
+		
+	}
 
-}
